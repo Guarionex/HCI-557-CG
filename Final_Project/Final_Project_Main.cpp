463b221d507686ee5a6d343c31012df03a88ae09
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-12-10 00:32:05 -0600

    Set scales

--- Final_Project/Final_Project_Main.cpp
+++ Final_Project/Final_Project_Main.cpp
@@ -103,28 +103,28 @@ void Init()
 	{
 		vec3(10.0f, 0.0f, 0.0f),
 		vec3(1.0, -1.0, -1.0),
-		vec3(1.0f, 1.0f, 1.0f)
+		vec3(2.0f, 2.0f, 2.0f)
 	};
 
 	InitialTransform initial_transform3
 	{
 		vec3(3.0f, 4.0f, 14.0f),
 		vec3(-1.0, 1.0, 0.0),
-		vec3(1.0f, 1.0f, 1.0f)
+		vec3(3.0f, 3.0f, 3.0f)
 	};
 	
 	InitialTransform initial_transform4
 	{
 		vec3(20.0f, 4.0f, -13.0f),
 		vec3(-1.0, 1.0, -1.0),
-		vec3(1.0f, 1.0f, 1.0f)
+		vec3(4.0f, 4.0f, 4.0f)
 	};
 
 	InitialTransform initial_transform5
 	{
 		vec3(-16.0f, 7.0f, 0.0f),
 		vec3(1.0, -1.0, 1.0),
-		vec3(1.0f, 1.0f, 1.0f)
+		vec3(5.0f, 5.0f, 5.0f)
 	};
 
 	ShaderFiles pbr_shader
