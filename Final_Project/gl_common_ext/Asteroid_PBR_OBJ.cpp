f0779413c30929fe2937390d8a9773ea05f4fbab
Edwin O. Martinez Velazquez <chespirana@gmail.com>
2018-12-09 19:26:37 -0600

    Blank Asteroid class

---
+++ Final_Project/gl_common_ext/Asteroid_PBR_OBJ.cpp
@@ -0,0 +1,12 @@
+#include "Asteroid_PBR_OBJ.h"
+
+
+
+Asteroid_PBR_OBJ::Asteroid_PBR_OBJ()
+{
+}
+
+
+Asteroid_PBR_OBJ::~Asteroid_PBR_OBJ()
+{
+}
---
+++ Final_Project/gl_common_ext/Asteroid_PBR_OBJ.h
@@ -0,0 +1,8 @@
+#pragma once
+class Asteroid_PBR_OBJ
+{
+public:
+	Asteroid_PBR_OBJ();
+	~Asteroid_PBR_OBJ();
+};
+
